49fe110ed795bb46ac372df071b3591f9e443a3b
Patricio <wearetamo@gmail.com>
2019-05-26 11:12:38 -0400

    displaying results

--- src/script.js
+++ src/script.js
@@ -1,25 +1,29 @@
 'use-strict';
 
-function parenthesesAreBalanced() {
+function validLISPCode() {
   const str = document.getElementById('stringToTest').value;
-  let stack = [];
-  const OPEN_BRACKET = '(';
-  const CLOSE_BRACKET = ')';
-  let i;
-  for (i = 0; i <= str.length; i++) {
-    let char = str[i];
-    if (OPEN_BRACKET === char) {
-      stack.push(char);
-    } else if (CLOSE_BRACKET === char) {
-      if (stack[stack.length - 1] === OPEN_BRACKET) {
-        stack.pop();
-      } else {
+  if (str !== null && str !== '' && str !== undefined) {
+    let stack = [];
+    const OPEN_BRACKET = '(';
+    const CLOSE_BRACKET = ')';
+    let i;
+    for (i = 0; i <= str.length; i++) {
+      let char = str[i];
+      if (OPEN_BRACKET === char) {
         stack.push(char);
-        break;
+      } else if (CLOSE_BRACKET === char) {
+        if (stack[stack.length - 1] === OPEN_BRACKET) {
+          stack.pop();
+        } else {
+          stack.push(char);
+          break;
+        }
       }
     }
+    document.getElementById('stringToTest').value = '';
+    document.getElementById('res-value').innerHTML = stack.length === 0;
+  } else {
+    document.getElementById('stringToTest').value = '';
+    document.getElementById('res-value').innerHTML = true;
   }
-  console.log(stack.length === 0);
-  document.getElementById('stringToTest').value = '';
-  return stack.length === 0;
 }
